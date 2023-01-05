f3fe0a99d1b6ff0a5fa32b5fba4e068b7ee5ca89
Joshua George <horheyinc8@gmail.com>
2023-01-05 22:02:13 +0100

    benito updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,36 +37,15 @@ function Home() {
       <div className="section" id="about">
         <img src={ericeira} alt="ericeira" className="image" />
         <div className="section-heading">
-          <h2>About Benito</h2>
+          <h2>About Myself:</h2>
           <p>
-            Originally from a small town in Germany, Benito found his place the
-            second that he arrived in Ericeira. Great food, peaceful energy and
-            amazing people from all over the world. He knew this would become
-            his home, and the friends he has made here will be his family. After
-            returning several times, he finally made the permanent move to
-            Ericeira and soon he found his calling of helping people through
-            massage. He heals their body with his hands and with his words and
-            energy... he heals their soul. Other than massage, Benito loves to
-            cook for his friends and family and he loves fitness and health.
-            Basically anyting that promotes healthy, happy people... he is a
-            strong supporter.
+            I am a certified massage therapist, who has a lot of passion and
+            joy. It makes me so happy and grateful to see that my patients are
+            just relaxed and feel more comfortable in thier bodies.
           </p>
         </div>
       </div>
-      <div className="section">
-        <img src={town} alt="ericeira" className="image" />
-        <div className="section-heading">
-          <h2>Location:</h2>
-          <p className="paragragh" id="location">
-            Located in beautiful Ericeira, Portgual. There is no better way to
-            relax after a long day at the beach, or after a grueling surf
-            session than with an amazing massage by Benito. Trained and
-            certified at one of the most renowned massage schools in Lisbon, He
-            will devout his full attention to all of your body's needs during
-            your massage.
-          </p>
-        </div>
-      </div>
+
       <div className="section" id="contact">
         <div className="section-heading">
           <h2>Contact</h2>
